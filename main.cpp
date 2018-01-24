bf7dcf368aec9e8ea42d77812ba67be42dff1e0a
ptam1 <puitm3@gmail.com>
2018-01-23 23:58:45 -0500

    Fixed Main

--- main.cpp
+++ main.cpp
@@ -6,34 +6,40 @@ using namespace std;
 int main()
 {
 	sequence test;
-	string user_input;
+	char user_input;
+	char exit = 'E';
 	double insert_input;
 	double attach_input;
 	
-	cout << "Commands: Start, Advance, Insert, Attach, Remove, Size, Find Item, Current Item" << endl;
+	cout << "Commands: !: Start, +: Advance, I: Insert, A: Attach, R: Remove, S: Size, ?: Is Item, C: Current Item, E: Exit" << endl;
 	cout << "Enter Your Command:" << endl;
-	getline(cin, user_input);
+	cin >> user_input;
 	
+	while(user_input != exit)
+	{
 	switch(user_input)
 	{
-		case 'Start': test.start();
+		case '!': test.start();
 					   break;
-		case "Advance": test.advance();
+		case '+': test.advance();
 						 break;
-		case "Insert": cout << "What Number Would You Like To Insert?" << endl;
-						getline(cin, insert_input);
+		case 'I': cout << "What Number Would You Like To Insert?" << endl;
+						cin >> insert_input;
 						test.insert(insert_input);
-		case "Attach": cout << "What Number Would You Like To Attach?" << endl;
-						getline(cin, attach_input);
+		case 'A': cout << "What Number Would You Like To Attach?" << endl;
+						cin >> attach_input;
 						test.attach(attach_input);
-		case "Remove": cout << "Testing remove_current Member Function" << endl;
+		case 'R': cout << "Testing remove_current Member Function" << endl;
 						 test.remove_current();
-		case "Size": cout << "Testing size Member Function" << endl;
+		case 'S': cout << "Testing size Member Function" << endl;
 							  cout << "The Sequence Size is: " << " " << test.size() << endl;
-		case "Find Item": cout << "Testing is_item Member Function" << endl;
+		case '?': cout << "Testing is_item Member Function" << endl;
 								   test.is_item();
-		case "Current Item": cout << "Testing current Member Function" << endl;
+		case 'C': cout << "Testing current Member Function" << endl;
 							  cout << "The Current Item is: " << " " << test.current() << endl;
+	}
+	cout << "Enter Another Command" << endl;
+	cin >> user_input;
 	}
 	return 0;
 }
